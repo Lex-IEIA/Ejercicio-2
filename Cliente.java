c493ff2d209c37dfbd609fb3df071d7e1a926828
Alex <alex.analuisa@epn.edu.ec>
2025-07-23 20:44:21 -0500

    Revision

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,38 @@
 package Ejercicio;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Cliente {
 	int id;
 	String nombre, telefono;
+	private List<Cuenta> cuentas = new ArrayList();
+	private List<Transaccion> transacciones = new ArrayList();
 	
-	public void deposito() {
+	
+	public Cliente(int id, String nombre, String telefono) {
+		this.id = id;
+		this.nombre = nombre;
+		this.telefono = telefono;
 		
 	}
-	public void retiro
-
+	
+	public void agregarCuenta(Cuenta cuenta) {
+		cuentas.add(cuenta);
+	}
+	
+	public void registrarTransaccion(Transaccion transaccion) {
+		transacciones.add(transaccion);
+	}
+	
+	public void deposito(Cuenta cuenta, double monto) {
+		cuenta.deposito(monto);
+		registrarTransaccion(new Transaccion(TipoTransaccion.Deposito, monto, Hora.getInstancia().today()));
+	}
+	
+	public void retiro(Cuenta cuenta, double monto) {
+		cuenta.retiro(monto);
+		registrarTransaccion(new Transaccion(TipoTransaccion.Retiro, monto, Hora.getInstancia().today()));
+		
+	}
 }
--- Transaccion.java
+++ Transaccion.java
@@ -1,5 +1,19 @@
 package Ejercicio;
+import java.time.LocalTime;
+
 
 public class Transaccion {
-
+	TipoTransaccion tipo;
+	double monto;
+	LocalTime fecha;
+	
+	
+	public Transaccion(TipoTransaccion tipo, double monto, LocalTime fecha) {
+		this.tipo = tipo;
+		this.monto = monto;
+		this.fecha = fecha;
+	}
+	
+	
+	
 }
